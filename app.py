1ea01588f6be968f74ae3f6f83a10fc758cc4696
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2021-03-30 18:19:47 -0300

    Adjusting log settings. Adding Book API.

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 # Build-in modules
+import configparser
 import logging
 from multiprocessing import cpu_count as cpu
+from queue import Queue
 
 # Added modules
+from telegram.ext import Updater, MessageHandler, Filters
 
 # Project modules
 
@@ -28,8 +31,74 @@ else:
 logger = logging.getLogger(__name__)
 
 
+class TelegramMessageQueue(object):
+    """Incoming messages Queue initializer"""
+
+    def __init__(self):
+        """
+        Initial state
+        """
+        self.message_queue = Queue()
+
+
+class InitializeTelegram(object):
+    """Telegram Bot initializer"""
+
+    def __init__(self):
+        # Configuring bot
+        config = configparser.ConfigParser()
+        config.read_file(open('config.ini'))
+        if WORK_MODE == 'dev':
+            telegram_token = config['DEV']['token']
+        else:
+            # 'prod'
+            telegram_token = config['DEFAULT']['token']
+
+        # Telegram incoming messages Queue initializer
+        self.queue = Queue()
+
+        # Connecting to Telegram API
+        self.updater = Updater(token=telegram_token, use_context=True)
+        dispatcher = self.updater.dispatcher
+
+        # Creating handlers
+        msg_handler = MessageHandler(Filters.text, lambda update, context: telegram_message(update, self.queue))
+
+        # Message handler must be the last one
+        dispatcher.add_handler(msg_handler)
+
+        # log all errors
+        dispatcher.add_error_handler(error)
+
+        # and then, start pulling for new messages
+        self.updater.start_polling(drop_pending_updates=True)
+
+        while not self.updater.running:
+            pass
+
+
 def application():
     """" All application has its initialization from here """
     logger.info('Main application is running!')
     # Count available CPU Cores
     logger.debug("Number of cpu: %s", cpu())
+
+    # Initializing Telegram
+    _telegram = InitializeTelegram()
+
+
+def telegram_message(update, sys_queue):
+    """All received Telegram messages is queued here"""
+    try:
+        logger.debug('{}'.format(update.message.chat_id))
+
+        # Message Queue
+        sys_queue.put(update)
+        logger.info('[Message queue: {}]'.format(int(sys_queue.qsize())))
+    except Exception as e:
+        logger.exception('{}'.format(e), exc_info=False)
+
+
+def error(update, context):
+    """Log Errors caused by Updates"""
+    logger.error('Update "%s" caused error "%s"', update, context.error)
