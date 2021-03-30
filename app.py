2c7fc39d22e8dcc29aa269cf5ac8f1e376dfd9f0
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2021-03-30 18:00:03 -0300

    Adjusting log settings. Adding Book API.

--- app.py
+++ app.py
@@ -1,73 +1,35 @@
 # Build-in modules
 import logging
-import time
-from datetime import timedelta
-from threading import ThreadError, Thread
+from multiprocessing import cpu_count as cpu
 
 # Added modules
-from pytictoc import TicToc
 
 # Project modules
 
-# Print in file
-# logging.basicConfig(filename='logs.log',
-#                     filemode='w',
-#                     level=logging.INFO,
-#                     format='%(asctime)s | %(process)d | %(name)s | %(levelname)s:  %(message)s',
-#                     datefmt='%d/%b/%Y - %H:%M:%S')
+""" ----  Mode work options ------
+Development    = 'dev'
+Production     = 'prod'
+------------------------------ """
+WORK_MODE = 'dev'
 
-# Print in software terminal
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s | %(process)d | %(name)s | %(levelname)s:  %(message)s',
-                    datefmt='%d/%b/%Y - %H:%M:%S')
+if WORK_MODE == 'prod':
+    # Print in file
+    logging.basicConfig(filename='logs.log',
+                        filemode='w',
+                        level=logging.INFO,
+                        format='%(asctime)s | %(process)d | %(name)s | %(levelname)s:  %(message)s',
+                        datefmt='%d/%b/%Y - %H:%M:%S')
+else:
+    # Print in software terminal
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s | %(process)d | %(name)s | %(levelname)s:  %(message)s',
+                        datefmt='%d/%b/%Y - %H:%M:%S')
 
 logger = logging.getLogger(__name__)
-
-
-class ElapsedTime(object):
-    """
-    Measure the elapsed time between Tic and Toc
-    """
-    def __init__(self):
-        self.t = TicToc()
-        self.t.tic()
-
-    def elapsed(self):
-        _elapsed = self.t.tocvalue()
-        d = timedelta(seconds=_elapsed)
-        logger.info('< {} >'.format(d))
-
-
-class ThreadingProcessQueue(object):
-    """
-    The run() method will be started and it will run in the background
-    until the application exits.
-    """
-
-    def __init__(self, interval):
-        """
-        Constructor
-        """
-        self.interval = interval
-
-        thread = Thread(target=run, args=(self.interval,), name='Thread_name')
-        thread.daemon = True  # Daemonize thread
-        thread.start()  # Start the execution
-
-
-def run(interval):
-    """ Method that runs forever """
-    while True:
-        try:
-            time.sleep(interval)
-
-        except ThreadError as e:
-            logger.exception('{}'.format(e))
-
-        finally:
-            pass
 
 
 def application():
     """" All application has its initialization from here """
     logger.info('Main application is running!')
+    # Count available CPU Cores
+    logger.debug("Number of cpu: %s", cpu())
