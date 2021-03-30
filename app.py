d685fb52dfef74cfef0358e8d8ed15a3e583f6ab
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2021-03-30 18:45:55 -0300

    Telegram running.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import configparser
 import logging
 from multiprocessing import cpu_count as cpu
 from queue import Queue
+from threading import Thread
 
 # Added modules
 from telegram.ext import Updater, MessageHandler, Filters
@@ -20,25 +21,17 @@ if WORK_MODE == 'prod':
     logging.basicConfig(filename='logs.log',
                         filemode='w',
                         level=logging.INFO,
-                        format='%(asctime)s | %(process)d | %(name)s | %(levelname)s:  %(message)s',
+                        format='%(asctime)s | %(process)d | %(name)s | %(thread)d | %(threadName)s | %(levelname)s: '
+                               '%(message)s',
                         datefmt='%d/%b/%Y - %H:%M:%S')
 else:
     # Print in software terminal
     logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s | %(process)d | %(name)s | %(levelname)s:  %(message)s',
+                        format='%(asctime)s | %(process)d | %(name)s | %(thread)d | %(threadName)s | %(levelname)s: '
+                               '%(message)s',
                         datefmt='%d/%b/%Y - %H:%M:%S')
 
 logger = logging.getLogger(__name__)
-
-
-class TelegramMessageQueue(object):
-    """Incoming messages Queue initializer"""
-
-    def __init__(self):
-        """
-        Initial state
-        """
-        self.message_queue = Queue()
 
 
 class InitializeTelegram(object):
@@ -55,14 +48,14 @@ class InitializeTelegram(object):
             telegram_token = config['DEFAULT']['token']
 
         # Telegram incoming messages Queue initializer
-        self.queue = Queue()
+        self.msg_queue = Queue()
 
         # Connecting to Telegram API
         self.updater = Updater(token=telegram_token, use_context=True)
         dispatcher = self.updater.dispatcher
 
         # Creating handlers
-        msg_handler = MessageHandler(Filters.text, lambda update, context: telegram_message(update, self.queue))
+        msg_handler = MessageHandler(Filters.text, lambda update, context: telegram_message(update, self.msg_queue))
 
         # Message handler must be the last one
         dispatcher.add_handler(msg_handler)
@@ -77,8 +70,24 @@ class InitializeTelegram(object):
             pass
 
 
+class ProcessIncomingMessages(Thread):
+    """Start processing all Telegram incoming messages"""
+
+    def __init__(self, telegram_obj):
+        self.msg_queue = telegram_obj.msg_queue
+        Thread.__init__(self, name='Message dispatcher', args=())
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        """Process messages"""
+        while True:
+            self.msg_queue.get()
+            logger.info('[Message queue: {}]'.format(self.msg_queue.qsize()))
+
+
 def application():
-    """" All application has its initialization from here """
+    """All application has its initialization from here"""
     logger.info('Main application is running!')
     # Count available CPU Cores
     logger.debug("Number of cpu: %s", cpu())
@@ -86,15 +95,18 @@ def application():
     # Initializing Telegram
     _telegram = InitializeTelegram()
 
+    # Start processing all Telegram messages
+    ProcessIncomingMessages(_telegram)
 
-def telegram_message(update, sys_queue):
+
+def telegram_message(update, msg_queue):
     """All received Telegram messages is queued here"""
     try:
         logger.debug('{}'.format(update.message.chat_id))
 
         # Message Queue
-        sys_queue.put(update)
-        logger.info('[Message queue: {}]'.format(int(sys_queue.qsize())))
+        msg_queue.put(update)
+        logger.info('[Message queue: {}]'.format(int(msg_queue.qsize())))
     except Exception as e:
         logger.exception('{}'.format(e), exc_info=False)
 
